e03cab369ca5dbefab46e904a806e356ea5d6d19
j3toler <jay.toler@hotmail.com>
2017-12-04 22:36:03 -0800

    Finished parts of the project. Still need to finish FileReader, PaySlip, and Program.

--- CSProject/CSProject/Program.cs
+++ CSProject/CSProject/Program.cs
@@ -25,7 +25,6 @@ namespace CSProject
                 }
             }
         }
-
         private int hWorked
         {
             get
@@ -44,23 +43,94 @@ namespace CSProject
             }
         }
 
-        public float TotalPay { get; private set; }
+        public float TotalPay { get; protected set; }
         public float BasicPay { get; private set; }
         public string NameOfStaff { get; private set; }
-
-        void CalculatePay()
+        public int HoursWorked
         {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    hWorked = 0;
+                } else
+                {
+                    hWorked = value;
+                }
+            }
+        }
 
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating pay...");
+
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+        public override string ToString()
+        {
+            return "Name: " + NameOfStaff + ";\nType: " + "Staff" + ";\nHourly Rate: " + hourlyRate + ";\nHours Worked: " + HoursWorked + ";\nBase Pay: " + BasicPay + "\nTotal Pay: " + TotalPay + ";";
         }
     }
 
     class Manager : Staff
     {
+        private const float managerHourlyRate = 50;
+
+        public int Allowance { get; private set; }
+
+        public Manager(string name) : base(name, managerHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 80)
+            {
+                Allowance = 1000;
+                TotalPay = BasicPay + Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + NameOfStaff + ";\nType: " + "Manager" + ";\nHourly Rate: " + managerHourlyRate + ";\nHours Worked: " + HoursWorked + ";\nBase Pay: " + BasicPay + ";\nAllowance: " + Allowance + ";\nTotal Pay: " + TotalPay + ";";
+        }
 
     }
 
     class Admin : Staff
     {
+        private const float overtimeRate = 15.5F;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name) : base(name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (160 - HoursWorked);
+                TotalPay += Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + NameOfStaff + ";\nType: " + "Admin" + ";\nHourly Rate: " + adminHourlyRate + ";\nHours Worked: " + HoursWorked + ";\nOvertime Rate:" + overtimeRate + ";\nBase Pay: " + BasicPay +  ";\nTotal Pay: " + TotalPay + ";";
+        }
 
     }
 
